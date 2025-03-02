f3c019482373eff24f15c240bd15768fbf307f6b
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 21:25:15 +0800

    sss

--- final_project/src/components/ExperienceSection.vue
+++ final_project/src/components/ExperienceSection.vue
@@ -1,61 +1,97 @@
 <template>
-  <section id="experience">
-    <br>
-    <p class="section__text__p1">Explore More</p>
-    <h1 class="title">Information About Me</h1>
-    <div class="experience-details-container">
-      <div class="about-containers">
-        <div class="details-container">
-          <h2 class="experience-sub-title">Hobbies and Interests</h2>
-          <div class="article-container">
-            <article v-for="(hobby, index) in hobbies" :key="index">
-              <img :src="hobby.image" alt="Experience icon" class="icon" />
-              <div>
-                <h3>{{ hobby.title }}</h3>
-                <p>{{ hobby.description }}</p>
-              </div>
-            </article>
+     <section id="experience">
+      <br>
+      <p class="section__text__p1">Explore More</p>
+      <h1 class="title">Information About Me</h1>
+      <div class="experience-details-container">
+        <div class="about-containers">
+          <div class="details-container">
+            <h2 class="experience-sub-title">Hobbies and Interests</h2>
+            <div class="article-container">
+              <article>
+                <v-img
+                  src="./public/images/cam.png"
+                  alt="Experience icon"
+                  class="icon"
+                ></v-img>
+                <div>
+                  <h3>Photography</h3>
+                  <p>Nature, landscapes, portraits, etc...</p>
+                </div>
+              </article>
+              <article>
+                <v-img
+                  src="./public/images/vb.png"
+                  alt="Experience icon"
+                  class="icon"
+                ></v-img>
+                <div>
+                  <h3>Volleyball</h3>
+                  <p>Bench Warmer or Waterboy</p>
+                </div>
+              </article>
+              <article>
+                <v-img
+                  src="./public/images/read.png"
+                  alt="Experience icon"
+                  class="icon"
+                ></v-img>
+                <div>
+                  <h3>Reading</h3>
+                  <p>Manhwas and Mangas</p>
+                </div>
+              </article>
+              <article>
+                <v-img
+                  src="./public/images/games.png"
+                  alt="Experience icon"
+                  class="icon"
+                ></v-img>
+                <div>
+                  <h3>Computer Games</h3>
+                  <p>ANY</p>
+                </div>
+              </article>
+              <article>
+                <v-img
+                  src="./public/images/crafts.png"
+                  alt="Experience icon"
+                  class="icon"
+                ></v-img>
+                <div>
+                  <h3>Arts and Crafts</h3>
+                  <p>Handmade Flowers, Letters, etc...</p>
+                </div>
+              </article>
+            </div>
           </div>
-        </div>
-        <div class="details-container">
-          <h2 class="experience-sub-title">More Information</h2>
-          <div class="article-container">
-            <article v-for="(info, index) in moreInfo" :key="index">
-              <div>
-                <h3>{{ info.title }}</h3>
-                <p>{{ info.description }}</p>
-              </div>
-            </article>
+          <div class="details-container">
+            <h2 class="experience-sub-title">More Information</h2>
+            <div class="article-container">
+              <article>
+                <div>
+                  <h3>IT Experience</h3>
+                  <p>I had no prior experience with IT </p>
+                </div>
+              </article>
+              <article>
+                <div>
+                  <h3>Goals</h3>
+                  <p>My Goal is To be rich enough that no one from my family would work.</p>
+                </div>
+              </article>
+              <article>
+                <div>
+                  <h3>Dreams</h3>
+                  <p>There is one dream that I will never let go, that is to be able to afford to give to donate to everyone. (and to have a big house for my family)</p>
+                </div>
+              </article>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
 </template>
 
 <script>
-import img1 from '@/public/images/cam.png';
-import img2 from '@/public/images/vb.png';
-import img3 from '@/public/images/read.png';
-import img4 from '@/public/images/games.png';
-import img5 from '@/public/images/crafts.png';
-
-export default {
-  data() {
-    return {
-      hobbies: [
-        { title: "Photography", description: "Nature, landscapes, portraits, etc...", image: img1 },
-        { title: "Volleyball", description: "Bench Warmer or Waterboy", image: img2 },
-        { title: "Reading", description: "Manhwas and Mangas", image: img3 },
-        { title: "Computer Games", description: "ANY", image: img4 },
-        { title: "Arts and Crafts", description: "Handmade Flowers, Letters, etc...", image: img5 },
-      ],
-      moreInfo: [
-        { title: "IT Experience", description: "I had no prior experience with IT" },
-        { title: "Goals", description: "My Goal is to be rich enough that no one from my family would work." },
-        { title: "Dreams", description: "I dream of being able to donate to everyone and to have a big house for my family." },
-      ],
-    };
-  },
-};
 </script>
