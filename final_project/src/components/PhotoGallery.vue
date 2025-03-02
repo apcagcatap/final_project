9e98b01c1779ede2f4f66db00aea3a4ccdfd6155
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 18:56:41 +0800

    sss

--- final_project/src/components/PhotoGallery.vue
+++ final_project/src/components/PhotoGallery.vue
@@ -1,38 +1,63 @@
-Vue.component('guestbook-form', {
-    data: function() {
+<template>
+    <section id="projects">
+      <p class="section__text__p1">Look At My</p>
+      <h1 class="title">Photo Gallery</h1>
+      <div class="experience-details-container">
+        <div class="about-containers">
+          <div class="details-container color-container">
+            <div class="article-container">
+              <div id="appp">
+                <photo-carousel></photo-carousel>
+              </div>
+            </div>
+            <h2 class="experience-sub-title project-title">MY FAVORITE PICTURES</h2>
+          </div>
+        </div>
+      </div>
+</template>
+
+<script>
+Vue.component('photo-carousel', {
+    data: function () {
+      return {
+        images: [
+          'https://apcagcatap.github.io/WEBPROG-Personal-Website/home/IMG_0245.JPG',
+          'https://apcagcatap.github.io/WEBPROG-Personal-Website/home/IMG_0637.JPG',
+          'https://apcagcatap.github.io/WEBPROG-Personal-Website/home/IMG_1230.JPG',
+          'https://apcagcatap.github.io/WEBPROG-Personal-Website/home/IMG_1554.JPG',
+        ],
+        currentIndex: 0,
+      };
+    },
+    computed: {
+      transformStyle() {
+        return `translateX(-${this.currentIndex * 100}%)`;
+      },
     },
     methods: {
-      submitForm: function() {
-      }
+      nextImage() {
+        this.currentIndex = (this.currentIndex + 1) % this.images.length;
+      },
+      prevImage() {
+        this.currentIndex =
+          (this.currentIndex - 1 + this.images.length) % this.images.length;
+      },
     },
     template: `
-      <div class="form-container">
-        <h3>Guestbook / Survey / Comments</h3>
-        <form @submit.prevent="submitForm">
-          <label for="name">Name:</label>
-          <input type="text" id="name" v-model="name" required><br><br>
-
-          <label for="email">Email:</label>
-          <input type="email" id="email" v-model="email" required><br><br>
-
-          <label for="message">Message:</label>
-          <textarea id="message" v-model="message" required></textarea><br><br>
-
-          <button type="submit">Submit</button>
-        </form>
-
-        <h4>Guestbook Entries</h4>
-        <ul>
-          <li v-for="(entry, index) in entries" :key="index">
-            <strong>{{ entry.name }} ({{ entry.email }})</strong><br>
-            <p>{{ entry.message }}</p>
-          </li>
-        </ul>
+      <div class="carousel">
+        <div class="carousel-images" :style="{ transform: transformStyle }">
+          <img v-for="(image, index) in images" :key="index" :src="image" :alt="'Image ' + (index + 1)">
+        </div>
+        <div class="carousel-buttons">
+          <button @click="prevImage">❮</button>
+          <button @click="nextImage">❯</button>
+        </div>
       </div>
-    `
+    `,
   });
 
-  // Vue instance
+  // Vue Instance
   new Vue({
-    el: '#app',
+    el: '#appp',
   });
+</script>
