184ccedd23c1bbaaba1909026ce8864a0e3b5fcb
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 18:02:17 +0800

    d

--- final_project/src/App.vue
+++ final_project/src/App.vue
@@ -1,34 +1,15 @@
 <template>
   <div id="app">
-    <NavBar />
-    <ProfileSection />
-    <AboutSection />
-    <ExperienceSection />
-    <PhotoGallery />
-    <ContactSection />
-    <FooterComponent />
+    <NavBar/>
+    <ProfileSection/>
+    <AboutSection/>
+    <ExperienceSection/>
+    <PhotoGallery/>
+    <ContactSection/>
+    <FooterComponent/>
   </div>
 </template>
 
 <script>
-import NavBar from "./components/NavBar.vue";
-import ProfileSection from "./components/ProfileSection.vue";
-import AboutSection from "./components/AboutSection.vue";
-import ExperienceSection from "./components/ExperienceSection.vue";
-import PhotoGallery from "./components/PhotoGallery.vue";
-import ContactSection from "./components/ContactSection.vue";
-import FooterComponent from "./components/FooterComponent.vue";
-
-export default {
-  components: {
-    NavBar,
-    ProfileSection,
-    AboutSection,
-    ExperienceSection,
-    PhotoGallery,
-    ContactSection,
-    FooterComponent,
-  },
-};
 </script>
 
--- final_project/src/main.js
+++ final_project/src/main.js
@@ -1,10 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./assets/css/mystyle.css";
-import "./assets/css/mediaqueries.css";
+import { createApp } from 'vue'
 
-Vue.config.productionTip = false;
+import App from './App.vue'
+import NavBar from "./components/NavBar.vue";
+import ProfileSection from "./components/ProfileSection.vue";
+import AboutSection from "./components/AboutSection.vue";
+import ExperienceSection from "./components/ExperienceSection.vue";
+import PhotoGallery from "./components/PhotoGallery.vue";
+import ContactSection from "./components/ContactSection.vue";
+import FooterComponent from "./components/FooterComponent.vue";
 
-new Vue({
-  render: (h) => h(App),
-}).$mount("#app");
+const app = createApp(App)
+
+app.component('NavBar', NavBar)
+app.component('ProfileSection', ProfileSection)
+app.component('AboutSection', AboutSection)
+app.component('ExperienceSection', ExperienceSection)
+app.component('PhotoGallery', PhotoGallery)
+app.component('ContactSection', ContactSection)
+app.component('FooterComponent', FooterComponent)
+
+app.mount('#app')
