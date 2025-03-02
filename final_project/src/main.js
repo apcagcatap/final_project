b6e58f1333f8087cde940dcbc400e3afa4c535a5
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 18:08:35 +0800

    ddd

--- final_project/src/main.js
+++ final_project/src/main.js
@@ -6,7 +6,6 @@ import ProfileSection from "./components/ProfileSection.vue";
 import AboutSection from "./components/AboutSection.vue";
 import ExperienceSection from "./components/ExperienceSection.vue";
 import PhotoGallery from "./components/PhotoGallery.vue";
-import ContactSection from "./components/ContactSection.vue";
 import FooterComponent from "./components/FooterComponent.vue";
 
 const app = createApp(App)
@@ -16,7 +15,6 @@ app.component('ProfileSection', ProfileSection)
 app.component('AboutSection', AboutSection)
 app.component('ExperienceSection', ExperienceSection)
 app.component('PhotoGallery', PhotoGallery)
-app.component('ContactSection', ContactSection)
 app.component('FooterComponent', FooterComponent)
 
 app.mount('#app')
