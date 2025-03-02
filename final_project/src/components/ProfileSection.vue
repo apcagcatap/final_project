f6d38f18352bf8ddbeaa366014da233781e7fdca
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 19:08:39 +0800

    sss

--- final_project/src/components/ProfileSection.vue
+++ final_project/src/components/ProfileSection.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="profile">
     <div class="section__pic-container">
-      <img src="skating.png" alt="ANTONNE MIGUELLE CATAP's profile picture" />
+      <img src="@/assets/images/skating.png" alt="ANTONNE MIGUELLE CATAP's profile picture" />
     </div>
     <div class="section__text">
       <p class="section__text__p1">Hello, I'm</p>
@@ -10,13 +10,13 @@
       <p class="section__text__p3">from BSCS - SS231</p>
       <div id="socials-container">
         <img
-          src="linkedin.png"
+          src="@/assets/images/linkedin.png"
           alt="My LinkedIn profile"
           class="icon"
           @click="openLink('https://www.linkedin.com/in/antonne-miguelle-catap-4a5946288/')"
         />
         <img
-          src="github.png"
+          src="@/assets/images/github.png"
           alt="My Github profile"
           class="icon"
           @click="openLink('https://github.com/apcagcatap')"
