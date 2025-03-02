03374d584fc629719846cd5cc61e033f2939c3f9
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-02 19:04:31 +0800

    sss

--- final_project/src/components/ProfileSection.vue
+++ final_project/src/components/ProfileSection.vue
@@ -1,30 +1,28 @@
 <template>
-  <section id="profile">
-    <div class="section__pic-container">
-      <img src="@/assets/images/skating.png" alt="Antonne Miguelle Catap" />
-    </div>
-    <div class="section__text">
-      <p class="section__text__p1">Hello, I'm</p>
-      <h1 class="title">Antonne Miguelle Catap</h1>
-      <p class="section__text__p2">An Aspiring Software Developer</p>
-      <p class="section__text__p3">from BSCS - SS231</p>
-      <div id="socials-container">
-
+    <section id="profile">
+      <div class="section__pic-container">
+        <img src="skating.png" alt="ANTONNE MIGUELLE CATAP's profile picture" />
       </div>
-    </div>
+      <div class="section__text">
+        <p class="section__text__p1">Hello, I'm</p>
+        <h1 class="title">Antonne Miguelle Catap</h1>
+        <p class="section__text__p2">An Aspiring Software Developer</p>
+        <p class="section__text__p3">from BSCS - SS231</p>
+        <div id="socials-container">
+          <img
+            src="linkedin.png"
+            alt="My LinkedIn profile"
+            class="icon"
+            onclick="location.href='https://www.linkedin.com/in/antonne-miguelle-catap-4a5946288/'">
+          <img
+            src="github.png"
+            alt="My Github profile"
+            class="icon"
+            onclick="location.href='https://github.com/apcagcatap'">
+        </div>
   </section>
 </template>
 
 <script>
-export default {
-  methods: {
-    goTo(platform) {
-      const urls = {
-        linkedin: "https://www.linkedin.com/in/antonne-miguelle-catap-4a5946288/",
-        github: "https://github.com/apcagcatap",
-      };
-      window.open(urls[platform], "_blank");
-    },
-  },
-};
+
 </script>
