598bad966de4eb7545f94bda36b4d5361b9b1073
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-05 04:20:40 +0800

    aaa

--- final_project/src/components/ProfileSection.vue
+++ final_project/src/components/ProfileSection.vue
@@ -2,8 +2,7 @@
   <section id="profile">
     <div class="section__pic-container">
       <img
-      src="/images/logo.png"
-      class="about-pic">
+      src="/images/skating.png">
     </div>
     <div class="section__text">
       <p class="section__text__p1">Hello, I'm</p>
@@ -28,3 +27,53 @@
 
 <script>
 </script>
+
+<style>
+#profile {
+  display: flex;
+  justify-content: center;
+  gap: 5rem;
+  height: 80vh;
+}
+
+.section__pic-container {
+  display: flex;
+  height: 400px;
+  width: 400px;
+  margin: auto 0;
+}
+
+.section__text {
+  align-self: center;
+  text-align: center;
+}
+
+.section__text p {
+  font-weight: 600;
+}
+
+.section__text__p1 {
+  text-align: center;
+}
+
+.section__text__p2 {
+  font-size: 1.75rem;
+}
+
+.section__text__p3 {
+  font-size: 1rem;
+  margin-bottom: 1rem;
+}
+
+.title {
+  font-size: 3rem;
+  text-align: center;
+}
+
+#socials-container {
+  display: flex;
+  justify-content: center;
+  margin-top: 1rem;
+  gap: 1rem;
+}
+</style>
