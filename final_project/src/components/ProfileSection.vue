3f0713b7d84883f60a9d46468cacfbfc283fcd87
apcagcatap <agcatap@student.apc.edu.ph>
2025-03-05 04:16:39 +0800

    ss

--- final_project/src/components/ProfileSection.vue
+++ final_project/src/components/ProfileSection.vue
@@ -1,31 +1,29 @@
 <template>
-     <section id="profile">
-      <div class="section__pic-container">
-                        <img
-          src="/images/logo.png"
-          class="about-pic">
+  <section id="profile">
+    <div class="section__pic-container">
+      <img
+      src="/images/logo.png"
+      class="about-pic">
+    </div>
+    <div class="section__text">
+      <p class="section__text__p1">Hello, I'm</p>
+      <h1 class="title">Antonne Miguelle Catap</h1>
+      <p class="section__text__p2">An Aspiring Software Developer</p>
+      <p class="section__text__p3">from BSCS - SS231</p>
+      <div id="socials-container">
+        <img
+        src="/images/linkedin.png"
+        class="icon"
+        @click="goToLinkedIn"
+        >
+        <img
+        src="/images/github.png"
+        class="icon"
+        @click="goToLinkedIn"
+        >
       </div>
-      <div class="section__text">
-        <p class="section__text__p1">Hello, I'm</p>
-        <h1 class="title">Antonne Miguelle Catap</h1>
-        <p class="section__text__p2">An Aspiring Software Developer</p>
-        <p class="section__text__p3">from BSCS - SS231</p>
-        <div id="socials-container">
-          <img
-          src="/images/linkedin.png"
-            class="icon"
-            @click="goToLinkedIn"
-          >
-          
-          <img
-          src="/images/github.png"
-            class="icon"
-            @click="goToLinkedIn"
-          >
-         
-        </div>
-      </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script>
